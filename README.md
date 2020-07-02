c8894d5953f8c47200e4973794852c5b74345ff4
Angius <koumarin@gmail.com>
2020-07-02 21:17:19 +0200

    ## 1.2.0 - Updated README.md - Fixed issues with creating and reading the .cfg file on Windows - Fixed trying to fetch empty pages

--- README.md
+++ README.md
@@ -4,12 +4,17 @@
 
 ____
 
+## Download
+
+From the [releases page](https://github.com/Atulin/DerpiDL/releases/latest)
+
 ## Usage
 
 * `--key` or `-K` to pass your API key. You only need to supply it once, it's saved in `derpidl.cfg`
 * `--limit` or `-L` to set the limit of pages
 * Any other strings passed as arguments are treated as tags to search for
-* If you need to pass negated tags like `-fluttershy` enclose all tags in quotes
+* If you need to pass tags that contain non-alphanumeric characters like `-fluttershy` 
+or `my:upvotes` enclose all tags in quotes
 
 Search syntax is the same as on Depribooru/Philomena, that is `some tag, another tag`
 and not `some_tag another_tag`.
@@ -20,7 +25,7 @@ and not `some_tag another_tag`.
     * Downloads first 2 pages of images tagged with `applejack`, `rainbow dash` and `safe`.
 * `derpi-dl.exe "fluttershy, -rarity"`
     * Downloads all images tagged with `fluttershy` but not tagged with `rarity`.
-* `derpi-dl.exe -K <your derpibooru key> -L 5 "pinkie pie, flurry heart`
+* `derpi-dl.exe -K <your derpibooru key> -L 5 "pinkie pie, flurry heart"`
     * Saves `<your derpibooru key>` as the API key, and downloads 5 pages of images tagged with
     `pinkie pie` and `flurry heart`, applying your personal settings to the search.
 
--- src/main.dart
+++ src/main.dart
@@ -10,11 +10,11 @@ const api = 'https://derpibooru.org/api/v1/json/search/images';
 Future<void> main(List<String> arguments) async {
   var dio = Dio();
   var parser = ArgParser();
-  var cfg = File(join(dirname(Platform.script.path), '/derpidl.cfg'));
+  var cfg = File(trimLeading('/', join(dirname(Platform.script.path), 'derpidl.cfg')));
 
   // Parser config
   parser.addOption('key', abbr: 'K', help: 'Your API key from Derpibooru');
-  parser.addOption('limit', abbr: 'L', help: 'The amount of pages to parse', defaultsTo: '1');
+  parser.addOption('limit', abbr: 'L', help: 'The amount of pages to parse', defaultsTo: '0');
   var options = parser.parse(arguments);
   var key = options['key'];
   var limit = int.parse(options['limit']);
@@ -45,9 +45,11 @@ Future<void> main(List<String> arguments) async {
       'q': search,
       'page': idx++
     }).catchError((e) => print(e));
+    if (res.data['images'].length <= 0) break;
 
     // Divide fetched URLs amongst 5 lists
     for (var img in res.data['images']) {
+
       images[list_idx].add(img['view_url']);
 
       if (list_idx >= 4) {
@@ -71,4 +73,12 @@ Future<void> main(List<String> arguments) async {
   }
 
   print('All images downloaded.');
+}
+
+String trimLeading(String pattern, String from) {
+  var i = 0;
+  while (from.startsWith(pattern, i)) {
+    i += pattern.length;
+  }
+  return from.substring(i);
 }
